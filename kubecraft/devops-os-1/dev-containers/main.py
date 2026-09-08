36f71567b195f455b4cc53ae9eb8b1135890ff75
angel-n-chavez <achavez.devops@gmail.com>
2026-09-08 08:29:32 -0500

    work: added devcontainer dir for future projects/work

---
+++ kubecraft/devops-os-1/dev-containers/main.py
@@ -0,0 +1,8 @@
+def HelloWorld(name):
+    '''
+    function that takes a name and returns a string with the name appended to it
+    '''
+
+    print(f"Hello {name} nice to meet you!")
+
+HelloWorld("Angel")
