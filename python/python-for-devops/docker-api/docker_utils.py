2a1ec2acec0599820965a8494612e8144535135e
angel-n-chavez <achavez.devops@gmail.com>
2026-09-18 14:25:09 -0500

    feat/fix: /running-containers endpoint fixed now returns all containers ran, even if exited. AND successfully created a /run-alpine endpoint that simply runs a alpine docker container via POST with status message response

--- python/python-for-devops/docker-api/docker_utils.py
+++ python/python-for-devops/docker-api/docker_utils.py
@@ -1,8 +1,38 @@
 import docker
 
-def docker_metrics():
+client = docker.from_env()
+
+def docker_images():
+    """
+    function that returns a list of docker images on a system
+    """
+
     # instance of docker client to communicate with the docker daemon
-    client = docker.from_env()
     docker_images = client.images.list()
 
     return [image.tags for image in docker_images]
+
+def docker_containers():
+    """
+    function that returns a list of running containers
+    """
+    running_containers = client.containers.list(all=True)
+
+    return [
+        {
+            "id" : container.short_id,
+            "name" : container.name,
+            "status" : container.status,
+            "image" : container.image.tags[0] if container.image.tags else "None"
+        }
+        for container in running_containers
+    ]
+
+def run_hello_world():
+    """
+    function that runs alpine hello world docker container
+    """
+    container = client.containers.run('alpine','sleep 30', detach=True)
+    output_bytes = container.logs()
+
+    return output_bytes.decode("utf-8").strip()
--- python/python-for-devops/docker-api/main.py
+++ python/python-for-devops/docker-api/main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI
-from docker_utils import docker_metrics
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from docker_utils import docker_images, docker_containers, run_hello_world
 
 app = FastAPI(title="Docker Utils API")
 
-@app.get("/docker-metrics")
-def get_docker_metrics():
+@app.get("/docker-images")
+def get_docker_images():
     """
     This API gets the docker metrics from daemon running on my dev machine
     """
-    return docker_metrics()
+
+    return docker_images()
+
+@app.get("/running-containers")
+def get_running_containers():
+    """
+    This API gets a list of all of the running containers on my dev machine
+    """
+
+    return docker_containers()
+
+@app.post("/run-alpine")
+def run_container():
+    """
+    This API sends a request to run a basic alpine 'Hello World' container
+    """
+
+    try:
+        result = run_hello_world()
+        return {"status" : "sucess", "status" : result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
